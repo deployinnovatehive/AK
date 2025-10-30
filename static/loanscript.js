ab9cba86b292c9d289c130a646a71230bbca4169
deployinnovatehive <deploy.innovatehive@gmail.com>
2025-10-30 23:46:57 +0530

    Delete static directory

--- static/loanscript.js
+++
@@ -1,9 +0,0 @@
-// FAQ Accordion Logic
-const faqButtons = document.querySelectorAll('.faq-question');
-
-faqButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const answer = btn.nextElementSibling;
-    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-  });
-});
