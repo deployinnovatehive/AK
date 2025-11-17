27cbd3e013a51d33ebe277019afc583f00022384
deployinnovatehive <deploy.innovatehive@gmail.com>
2025-11-17 23:22:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/loan')
-def loan():
-    return render_template('loan.html')
-
-@app.route('/insurance')
-def insurance():
-    return render_template('Insurance.html')
-
-@app.route('/recruitment')
-def recruitment():
-    return render_template('recruitment.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/mice')
 def mice():
     return render_template('mice.html')
+
+@app.route('/insurance')
+def insurance():
+    return render_template('insurance.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
